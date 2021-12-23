40ec8551a0ff56888dfe8250de3c39595b4c0ec0
unknown <nghia.trvn@gmail.com>
2021-12-23 12:08:17 +0700

    firts upload code

---
+++ variable.cpp
@@ -0,0 +1,47 @@
+
+
+/*#include <iostream>
+using namespace std;
+
+int main() {
+    int myAge= 18;
+    cout << "I'm Andrew " << myAge << " years old.";
+    return 0;
+}
+
+//them cac bien khac nhau
+#include <iostream>
+using namespace std;
+    int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    cout << c;
+    return 0;
+} // namespace std;
+
+#include <iostream>
+using namespace std;
+ 
+int main ()
+{   
+    char Ten[200];
+    cout << "Nhap ten cua ban: ";
+    cin >> Ten;
+    cout << endl;
+    cout << "Ten ban vua nhap la: " << Ten << endl;
+}
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x;
+    cout << "Type a number:";
+    cin >> x;
+    cout << "Your number is: " << x;
+    return 0;
+}
